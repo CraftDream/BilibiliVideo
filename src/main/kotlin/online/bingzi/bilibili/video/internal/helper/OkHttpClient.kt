cc6b760f9a2f558b69734da8cb75589d99bcdbe8
梦幻星空 <47418954+CraftDream@users.noreply.github.com>
2024-05-30 23:51:13 +0800

    Update OkHttpClient.kt

--- src/main/kotlin/online/bingzi/bilibili/video/internal/helper/OkHttpClient.kt
+++ src/main/kotlin/online/bingzi/bilibili/video/internal/helper/OkHttpClient.kt
@@ -3,9 +3,6 @@ package online.bingzi.bilibili.video.internal.helper
 import okhttp3.OkHttpClient
 import online.bingzi.bilibili.video.internal.interceptor.ReceivedCookiesInterceptor
 import online.bingzi.bilibili.video.internal.interceptor.UserAgentInterceptor
-import taboolib.common.platform.function.pluginId
-import taboolib.common.platform.function.pluginVersion
-
 
 internal val client = OkHttpClient.Builder().addInterceptor(ReceivedCookiesInterceptor())
-    .addInterceptor(UserAgentInterceptor("MinecraftPlugin $pluginId/$pluginVersion(lhby233@outlook.com)")).build()
+    .addInterceptor(UserAgentInterceptor("BilibiliBot")).build()
