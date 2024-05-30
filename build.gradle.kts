40cc5799e333e94ca28e2503f37c2e40c17b91f8
梦幻星空 <47418954+CraftDream@users.noreply.github.com>
2024-05-30 22:46:47 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -68,11 +68,3 @@ configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
-
-kotlin {
-    sourceSets.all {
-        languageSettings {
-            languageVersion = "2.0"
-        }
-    }
-}
